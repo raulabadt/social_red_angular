5cb8ccb3924daf5b85b65fbb69fa108fe1148a9d
Raúl Abad <raulabadtorralba@gmail.com>
2024-06-14 11:25:20 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { PostComponent } from './post/post.component';
+import { CommentsComponent } from './comments/comments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegistroComponent },
+    { path: 'post', component: PostComponent },
+    { path: 'comments', component: CommentsComponent }
+];
