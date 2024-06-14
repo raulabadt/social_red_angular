8c0670e49a1ebfe5fac0f87d591e555c90e9d20b
Raúl Abad <raulabadtorralba@gmail.com>
2024-06-14 13:40:45 +0200

    Registro redirect.

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet,Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss'
 })
 export class RegistroComponent {
-
+  constructor(private router: Router) {}
+  redirectLogin(){
+    this.router.navigate(['/login']);
+  }
 }
