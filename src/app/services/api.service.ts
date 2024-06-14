240365d4c2c5f9c0f6e45a67c0a9fc1547f7d61d
Raúl Abad <raulabadtorralba@gmail.com>
2024-06-14 13:22:06 +0200

    Generate api service, comunicate symfony.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,29 @@
+// src/app/api.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private apiUrl = 'http://localhost:8000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getItems(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/items`);
+  }
+
+  addItem(item: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/items`, item);
+  }
+
+  updateItem(id: string, item: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/items/${id}`, item);
+  }
+
+  deleteItem(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/items/${id}`);
+  }
 }
